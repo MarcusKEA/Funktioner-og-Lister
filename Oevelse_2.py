7e327fb6ad8fbff01bdb296e6f7d643784d2100c
BuildTools <unconfigured@null.spigotmc.org>
2021-10-29 01:07:37 +0200

    Øvelse 1

---
+++ Oevelse_2.py
@@ -0,0 +1,7 @@
+def __main__():
+    
+
+
+if __name__ == "__main__":
+    __main__()
+     
--- funktion_lister.py
+++ funktion_lister.py
@@ -1,2 +1,26 @@
+import random
+
 def __main__():
-    print("Hello World!")
+    #1.1
+    items = ["apple","orange","banana","pear"]
+    items.append("strawberry")
+    print(items)
+
+    #1.2
+    print(items[0])
+    
+    #1.3
+    items[3] = "pineapple"
+    print(items)
+
+    #1.4
+    sameitems = ["a","b","c","d","d","d"]
+
+    #1.5
+    jokes = ["Jeg overvejer at gifte mig med en tysker er det over grænsen?","Alle børnene kom sikkert hjem fra fabrikken undtagen Ib og Arne de blev til chili konkarne"]
+    print(jokes[random.randrange(len(jokes))])
+
+
+if __name__ == "__main__":
+    __main__()
+     
